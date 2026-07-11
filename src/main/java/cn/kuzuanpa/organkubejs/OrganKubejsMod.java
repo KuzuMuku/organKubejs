a013d23050efb1de746c1a74876f27127552d626
kuzuanpa <misaka16384@outlook.com>
2026-07-11 12:15:50 +0800

    license

--- src/main/java/cn/kuzuanpa/organkubejs/OrganKubejsMod.java
+++ src/main/java/cn/kuzuanpa/organkubejs/OrganKubejsMod.java
@@ -1,3 +1,18 @@
+/*
+ * This class was created by <kuzuanpa>. It is distributed as
+ * part of the organKubejs Mod. Get the Source Code in github:
+ * https://github.com/KuzuMuku/organKubejs
+ *
+ * organKubejs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+
+ * organKubejs is Open Source and distributed under the
+ * AGPLv3 License: https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ */
+
 package cn.kuzuanpa.organkubejs;
 
 import cn.kuzuanpa.organkubejs.compat.OrganKubejsCompat;
